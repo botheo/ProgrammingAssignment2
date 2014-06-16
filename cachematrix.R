c620080611546104e6271102fa0bcef82b002ae9
Ondrej Bothe <botheo@seznam.cz>
2014-06-16 22:03:15 +0200

    Programmin 2 assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Commit Programming 2 Asigment
 
-## Write a short comment describing this function
+## Create a vector (containing functions) for the inverse matrix calcualtion.
+## Potentially calculate the inverse matrix in case that it is not cashed.
+##I did not solve the potentially issues with matrix (not possible to use solve function for the inverse matrix calculation)
+
+
+## Create a vector of function, that could be used for casching of inv matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calcualtion of inverse matrix itslefe, with condition on already existing (calcaulted) results
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
 }
